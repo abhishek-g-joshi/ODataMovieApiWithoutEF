a350cf8cd2f3a2742d520f3112389d7f4a06072c
Renuka Patil <69632043+renukapatil-21@users.noreply.github.com>
2022-03-22 20:26:32 +0530

    added dependencies and IEDM model

--- ODataMovieApiWithoutEF/Program.cs
+++ ODataMovieApiWithoutEF/Program.cs
@@ -1,8 +1,22 @@
+using Microsoft.AspNetCore.OData;
+using Microsoft.OData.Edm;
+using Microsoft.OData.ModelBuilder;
+using ODataMovieApiWithoutEF.Models;
+using ODataMovieApiWithoutEF.Repository;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+static IEdmModel GetEdmModel()
+{
+    ODataConventionModelBuilder builder1 = new ODataConventionModelBuilder();
+    builder1.EntitySet<Movie>("Movies");
+    var edmModel = builder1.GetEdmModel();
+    return edmModel;
+}
 
-builder.Services.AddControllers();
+builder.Services.AddControllers().AddOData(opt => opt.AddRouteComponents("v1", GetEdmModel()));
+builder.Services.AddScoped<IMovieRepository, MovieRepository>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
